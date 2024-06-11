4764d4061041aa71218ce448a90280d103182123
Davirain <davirain.yin@gmail.com>
2024-06-11 19:07:14 +0800

    add ch4

---
+++ python_crash_course/chapter4/even_numbers.py
@@ -0,0 +1,3 @@
+# range first is start number, second is end number, third is step
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
---
+++ python_crash_course/chapter4/numbers.py
@@ -0,0 +1,7 @@
+
+# range() function
+for value in range(1, 6):
+    print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
