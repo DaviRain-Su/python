e86870e8c2c7104cacf1f7adab4aa28d289f840c
Davirain <davirain.yin@gmail.com>
2024-06-12 12:19:26 +0800

    add ch9

---
+++ python_crash_course/ch9/electric_car.py
@@ -0,0 +1,34 @@
+from car import Car
+
+class Battery():
+
+    def __init__(self, battery_size: int = 75):
+        """ Initialize the battery's attributes. """
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """ Print a statement describing the battery size. """
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """ Print a statement about the range this battery provides. """
+        range = 0
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 85:
+            range = 270
+
+        message = f"This car can go approximately { range } miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    """ Represent aspects of a car, specific to electric vehicles. """
+
+    def __init__(self, make: str, model: str, year: int):
+        """ Initialize attributes of the parent class. """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """ Electric cars don't have gas tanks. """
+        print("This car doesn't need a gas tank!")
---
+++ python_crash_course/ch9/my_electric_car.py
@@ -0,0 +1,9 @@
+from car import Car, ElectricCar
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+#print(my_tesla.read_odometer())
+#my_tesla.describe_battery()
+#my_tesla.fill_gas_tank()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
