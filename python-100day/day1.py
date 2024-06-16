7cfdb7709eb70930e9c75921f5ae8508bcf738a5
Davirain <davirain.yin@gmail.com>
2024-06-16 23:47:13 +0800

    update

---
+++ python-100day/day1.py
@@ -0,0 +1,9 @@
+import sys
+
+print("Python version info: ", sys.version_info)
+print("Python path: ", sys.path)
+print("Python executable: ", sys.executable)
+print("Python prefix: ", sys.prefix)
+print("Python platform: ", sys.platform)
+print("Python implementation: ", sys.implementation)
+print("Python version: ", sys.version)
---
+++ python3-cookbook/ch1/mod.py
@@ -0,0 +1,5 @@
+p = (4, 5)
+print(f"p = {p}")
+x, y = p
+print(f"x = {x}")
+print(f"y = {y}")
