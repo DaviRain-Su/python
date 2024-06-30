41a65b65ab635fc06a7229a833d770dc8c4c7251
Davirain <davirain.yin@gmail.com>
2024-06-30 17:49:30 +0800

    remove something

--- python-100day/day1.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-print("Python version info: ", sys.version_info)
-print("Python path: ", sys.path)
-print("Python executable: ", sys.executable)
-print("Python prefix: ", sys.prefix)
-print("Python platform: ", sys.platform)
-print("Python implementation: ", sys.implementation)
-print("Python version: ", sys.version)
--- python3-cookbook/ch1/mod.py
+++
@@ -1,5 +0,0 @@
-p = (4, 5)
-print(f"p = {p}")
-x, y = p
-print(f"x = {x}")
-print(f"y = {y}")
